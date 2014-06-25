524b6bfa3b3cfea8759e30c9e848b09ed8a0b24e
Jared Sanson <jared@jared.geek.nz>
2014-06-25 21:37:25 +1200

    Basic framing protocol implementation
    Based on HLDC

---
+++ Control/HLDCParser.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace RGB.Control
+{
+    /// <summary>
+    /// High-Level Data Link Control Parser
+    /// http://en.wikipedia.org/wiki/High-Level_Data_Link_Control
+    /// </summary>
+    public class HLDCParser
+    {
+        private const byte HLDC_FRAME_DELIMITER = 0x7E;
+        private const byte HLDC_ESCAPE = 0x7D;
+        private const byte HLDC_ESCAPE_MASK = 0x20;
+
+        private static readonly int MAX_PACKET_LEN = 64;
+        private static readonly int HEADER_SIZE = Marshal.SizeOf(typeof(HeaderStruct));
+        private static readonly int FOOTER_SIZE = Marshal.SizeOf(typeof(HeaderStruct));
+        private static readonly int MIN_PACKET_LEN = HEADER_SIZE - FOOTER_SIZE;
+        private static readonly int MAX_PAYLOAD_LEN = MAX_PACKET_LEN - HEADER_SIZE - FOOTER_SIZE;
+
+
+        [StructLayout(LayoutKind.Sequential, Pack=1)]
+        private struct HeaderStruct
+        {
+            public byte command;
+            public byte length;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        private struct FooterStruct
+        {
+            public UInt16 crc;
+        }
+
+        private static byte[] StructToByteArray<T>(T str)
+        {
+            int size = Marshal.SizeOf(str);
+            byte[] arr = new byte[size];
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+
+            Marshal.StructureToPtr(str, ptr, true);
+            Marshal.Copy(ptr, arr, 0, size);
+            Marshal.FreeHGlobal(ptr);
+
+            return arr;
+        }
+
+        private static byte[] UnescapeBytes(byte[] buffer)
+        {
+            List<byte> resultBuffer = new List<byte>();
+            int i = 0;
+            while (i < buffer.Length)
+            {
+                if (buffer[i] == HLDC_ESCAPE)
+                    resultBuffer.Add((byte)(buffer[++i] ^ HLDC_ESCAPE_MASK));
+                else
+                    resultBuffer.Add(buffer[i]);
+
+                i++;
+            }
+            return resultBuffer.ToArray();
+        }
+
+        private static byte[] EscapeBytes(byte[] buffer)
+        {
+            List<byte> resultBuffer = new List<byte>();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                byte data = buffer[i];
+                if (data == HLDC_FRAME_DELIMITER || data == HLDC_ESCAPE)
+                {
+                    resultBuffer.Add(HLDC_ESCAPE);
+                    resultBuffer.Add((byte)(data ^ HLDC_ESCAPE_MASK));
+                }
+                else
+                    resultBuffer.Add(data);
+            }
+            return resultBuffer.ToArray();
+        }
+
+        public static byte[] CreatePacket<T>(byte command, T struc)
+        {
+            byte[] payload = StructToByteArray<T>(struc);
+            return CreatePacket(command, payload);
+        }
+
+        public static UInt16 CalculateCrc(byte[] buffer)
+        {
+            return 0;
+        }
+
+        public static byte[] CreatePacket(byte command, byte[] payload = null)
+        {
+            List<byte> packetBuilder = new List<byte>();
+
+            if (payload != null && payload.Length > MAX_PAYLOAD_LEN)
+                throw new Exception("HLDC payload too large");
+
+            packetBuilder.Add(HLDC_FRAME_DELIMITER);
+
+            // Build the packet header
+            HeaderStruct header = new HeaderStruct()
+            {
+                command = command,
+                length = (byte)((payload != null) ? payload.Length : 0)
+            };
+            packetBuilder.AddRange(EscapeBytes(StructToByteArray<HeaderStruct>(header)));
+
+            // Build the packet payload
+            UInt16 crc = 0;
+            if (payload != null)
+            {
+                byte[] escapedPayload = EscapeBytes(payload);
+                packetBuilder.AddRange(escapedPayload);
+                crc = CalculateCrc(escapedPayload);
+            }
+
+            // BUild the packet footer
+            FooterStruct footer = new FooterStruct()
+            {
+                crc = crc,
+            };
+            packetBuilder.AddRange(EscapeBytes(StructToByteArray<FooterStruct>(footer)));
+
+            packetBuilder.Add(HLDC_FRAME_DELIMITER);
+
+            if (packetBuilder.Count > MAX_PACKET_LEN)
+                throw new Exception("HLDC packet too large");
+
+            return packetBuilder.ToArray();
+        }
+
+        public static void ParsePacket(byte[] packet, out byte command, out byte[] payload)
+        {
+            payload = new byte[1];
+            command = 0;
+        }
+    }
+}
--- Control/HexController.cs
+++ Control/HexController.cs
@@ -19,6 +19,67 @@ namespace RGB.Control
         private int baud;
         private SerialPort serial;
 
+        #region Protocol Command Defs
+
+        private const byte CMD_POWER_ON = 0x01;
+        private const byte CMD_POWER_OFF = 0x02;
+        private const byte CMD_SET_MODE = 0x03;
+        private const byte CMD_GET_LAST_ERROR = 0x04;
+
+        private const byte CMD_SET_PWM = 0x10;
+        private const byte CMD_GET_PWM = 0x11;
+        private const byte CMD_SET_XYZ = 0x12;
+        private const byte CMD_GET_XYZ = 0x13;
+        private const byte CMD_SET_XYY = 0x14;
+        private const byte CMD_GET_XYY = 0x15;
+
+        private const byte CMD_SET_XYZ_CAL = 0x20;
+        private const byte CMD_GET_XYZ_CAL = 0x21;
+
+        private const byte CMD_ACK = 0xFF;
+
+        #endregion
+
+        #region Protocol Structs
+
+#pragma warning disable 0169 // Field XYZ is never used
+#pragma warning disable 0649 // Field XYZ is never assigned to, and will always have its default value XX
+
+        private struct PWMStruct
+        {
+            public Int16 CH1;  // Yes, signed ints. range is 0-32767, to match fractional Q15 datatype.
+            public Int16 CH2;
+            public Int16 CH3;
+            public Int16 CH4;
+        };
+
+        private struct XYZStruct
+        {
+            public float X;
+            public float Y;
+            public float Z;
+        };
+
+        private struct XYYStruct
+        {
+            public float x;
+            public float y;
+            public float Y;
+        }
+
+        private struct XYZCalStruct
+        {
+            public byte ch;
+            public XYYStruct point;
+        }
+
+#pragma warning restore 0169
+#pragma warning restore 0649
+
+        #endregion
+
+        #region Properties
+
         public override RGBColor Color
         {
             get { return color; }
@@ -39,36 +100,44 @@ namespace RGB.Control
             }
         }
 
+        #endregion
+
         private float limit(float val) {
             if (val < 0.0f) return 0.0f;
             if (val > 1.0f) return 1.0f;
             return val;
         }
 
+        private Int16 FloatToInt16(float x)
+        {
+            if (x < 0.0f) return 0;
+            if (x > 1.0f) return Int16.MaxValue;
+            return (Int16)(x * (float)Int16.MaxValue);
+        }
+
         private void Update()
         {
             RGBColor rgb = this.color;//= CIE1931.CorrectRGB(this.color);
 
-            var rbytes = BitConverter.GetBytes((float)limit(rgb.r));
-            var gbytes = BitConverter.GetBytes((float)limit(rgb.g));
-            var bbytes = BitConverter.GetBytes((float)limit(rgb.b));
-            var wbytes = BitConverter.GetBytes(0.0f);               // White channel
-            var lbytes = BitConverter.GetBytes((float)limit(brightness));         // Luminance
+            byte[] packet = HLDCParser.CreatePacket<PWMStruct>(0, new PWMStruct
+            {
+                CH1 = FloatToInt16(rgb.r),
+                CH2 = FloatToInt16(rgb.g),
+                CH3 = FloatToInt16(rgb.b),
+                CH4 = 0
+            });
 
+            //var packet = HLDCParser.CreatePacket(0x7E, new byte[] { });
+            /*foreach (var b in packet)
+                serial.Write(new byte[] { b }, 0, 1);*/
+            serial.Write(packet, 0, packet.Length);
 
-            List<byte> packet = new List<byte>();
-            packet.Add((byte)'H');
-            packet.Add(0); //padding
-            packet.Add(0); //padding
-            packet.Add(0); //padding
-            packet.AddRange(rbytes);
-            packet.AddRange(gbytes);
-            packet.AddRange(bbytes);
-            packet.AddRange(wbytes);
-            packet.AddRange(lbytes);
-
-            serial.Write(packet.ToArray(), 0, packet.Count);
+            byte[] buf = new byte[100];
+            serial.Read(buf, 0, buf.Length);
+            if (buf[0] != 0)
+                return;
         }
+
 
         public HexController(string port, int baud = 9600)
         {
