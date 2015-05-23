0c5e4e952f2d56feff20317b11adfc0613417ddc
Jared Sanson <jared@jared.geek.nz>
2015-05-24 02:30:44 +1200

    Added AnimEngine with simple hue cycle animation
    Also created a LogarithmicConverter to give greater dynamic range control over the speed

---
+++ Engines/AnimEngine/AnimEngine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using HexLight.Engine;
+using HexLight.Colour;
+
+namespace HexLight.Engine.Anim
+{
+    public enum Mode { Cycle };
+
+    [EngineName("Anim")]
+    public partial class AnimEngine : HexEngine
+    {
+        #region Properties
+
+        public Mode Mode { get; set; }
+
+        public double Speed { get; set; }
+
+        #endregion
+
+        public AnimEngine()
+        {
+            Mode = Mode.Cycle;
+            Speed = 0.1;
+        }
+
+        public override UserControl GetControlPage()
+        {
+            var page = new ControlPage();
+
+            page.DataContext = this;
+
+            // Allow parent to define width/height
+            page.Width = double.NaN;
+            page.Height = double.NaN;
+
+            return page;
+        }
+
+        public override void Enable()
+        {
+            
+        }
+
+        public override void Disable()
+        {
+            
+        }
+
+        public override RGBColor Update(double tickTime)
+        {
+            return CycleHueUpdate(tickTime);
+        }
+    }
+}
---
+++ Engines/AnimEngine/CycleAnimEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HexLight.Colour;
+
+namespace HexLight.Engine.Anim
+{
+    public partial class AnimEngine
+    {
+        private double cycleHue;
+
+        private HSVColor CycleHueUpdate(double tickRate)
+        {
+            cycleHue += Speed * tickRate;
+            return new HSVColor((float)cycleHue, 1.0f, 1.0f);
+        }
+    }
+}
